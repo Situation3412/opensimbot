43ec50e47e9aac6724e0dd6cd4547e9a1d6fe5a3
Humming Scandal <github.huskiness190@slmails.com>
2024-11-26 17:05:27 -0500

    Added electron

---
+++ public/electron.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+  });
+
+  // Load the index.html from a url in development
+  // and from a local file in production
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  // Open the DevTools in development mode
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+}); 
